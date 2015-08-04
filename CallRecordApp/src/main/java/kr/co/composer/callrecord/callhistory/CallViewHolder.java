89d30acc1cd8467f0adf0dfcededcdd6fa5c9bcc
composer10 <composer10@naver.com>
2015-08-04 14:52:12 +0900

    0804 make CallViewHolder

---
+++ CallRecordApp/src/main/java/kr/co/composer/callrecord/callhistory/CallViewHolder.java
@@ -0,0 +1,18 @@
+package kr.co.composer.callrecord.callhistory;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import kr.co.composer.callrecord.R;
+
+/**
+ * Created by composer on 2015-08-04.
+ */
+public class CallViewHolder {
+    ImageView imageView;
+    TextView name;
+    TextView time;
+    TextView sending;
+    TextView number;
+    TextView startTime;
+}
